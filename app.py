184dbce2d2c3bf24c36d83e77510ae3a69462de8
Isra Ragheb <israragheb@gmail.com>
2020-08-18 21:06:28 +0200

    Build a CURD with SQLAlchemy

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask,render_template,request,jsonify,abort
+
+from flask_sqlalchemy import SQLAlchemy
+import sys
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://israragheb@localhost:5432/todoapp'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key = True)
+    description = db.Column(db.String(), nullable = False)
+    def reper(self):
+        return f'<todo: {self.id}, {self.description}>'
+
+db.create_all()
+
+@app.route('/todos/create',methods=['post'])
+def create_todo():
+    error = False
+    body = {}
+    try:
+        descr = request.get_json()['description']
+        item = Todo(description=descr)
+        db.session.add(item)
+        db.session.commit()
+        body['description'] = item.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',data = Todo.query.all())
+
